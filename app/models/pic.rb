658dccf47866674dd4dec4dea461385bcca4864e
Jeffrey Dill <jeffdill2@gmail.com>
2013-11-23 19:49:46 -0500

    Hook up image uploading with carrierwave.

--- app/models/pic.rb
+++ app/models/pic.rb
@@ -1,2 +1,10 @@
 class Pic < ActiveRecord::Base
+
+	mount_uploader :image, ImageUploader
+
+	def helper_string
+		"#{self.lesson}, #{self.emotion}, #{self.learned}"
+		#self.lesson.to_s + ", " + self.emotion + ", " + self.learned
+	end
+
 end
